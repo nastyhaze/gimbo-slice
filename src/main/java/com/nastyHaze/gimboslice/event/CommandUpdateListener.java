1dc4c1546a8e18325c9846de5f86541af5e1b26f
nastyhaze <walkuhdan@gmail.com>
2022-02-27 19:03:12 -0500

    Initial commit - incomplete

---
+++ src/main/java/com/nastyHaze/gimboslice/event/CommandUpdateListener.java
@@ -0,0 +1,52 @@
+package com.nastyHaze.gimboslice.event;
+
+
+import com.nastyHaze.gimboslice.common.CommonConstant;
+import com.nastyHaze.gimboslice.entity.data.Command;
+import com.nastyHaze.gimboslice.repository.CommandRepository;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+/**
+ *  Handles updating Bot commands from Discord server Messages.
+ */
+@Service
+public class CommandUpdateListener extends MessageListener implements Listener<MessageCreateEvent> {
+
+    @Autowired
+    private CommandRepository commandRepository;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public Mono<Void> execute(MessageCreateEvent event) {
+        Mono<Void> stream;
+        String commandString = event.getMessage().getContent();
+
+        if(commandString.charAt(0) != CommonConstant.UPDATE_OPERATOR) {
+            stream = processUpdateCommand(event.getMessage(), null);
+        } else {
+            Optional<Command> incomingCommand =
+                    Optional.ofNullable(commandRepository.findByTriggerAndActiveTrue(commandString));
+
+            if (incomingCommand.isEmpty())
+                stream = processError(event.getMessage());
+                // stream = processPublicError(event.getMessage());
+            else
+                stream = processUpdateCommand(event.getMessage(), incomingCommand.get());
+        }
+
+        return stream;
+    }
+
+    @Override
+    public Class<MessageCreateEvent> getEventType() {
+        return MessageCreateEvent.class;
+    }
+}
