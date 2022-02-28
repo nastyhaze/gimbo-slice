ae41d0760eeda2cd935a6bb86586666ebaca28b5
nastyhaze <walkuhdan@gmail.com>
2022-02-27 19:03:58 -0500

    Completed logic for isServerOwner

--- src/main/java/com/nastyHaze/gimboslice/common/CommonUtility.java
+++ src/main/java/com/nastyHaze/gimboslice/common/CommonUtility.java
@@ -1,17 +1,19 @@
 package com.nastyHaze.gimboslice.common;
 
-
+import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.Member;
+import discord4j.discordjson.Id;
 import reactor.core.publisher.Mono;
+
 
 /**
  *  Utility methods used throughout the application. Not tied to specific operations.
  */
 public class CommonUtility {
 
-    public static boolean isServerOwner(Mono<Guild> server, Mono<Member> member) {
-        return false;
+    public static boolean isServerOwner(Mono<Guild> server, Id memberId) {
+        return server.map(guild -> guild.getOwnerId().equals(Snowflake.of(memberId)))
+                .blockOptional().orElse(false);
     }
 
     public static boolean isServerOwnerOrAdmin() {
