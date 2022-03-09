5ce4521dbd402d7e1258b853690f6238ead67bca
Mike <m.p.mckew@gmail.com>
2022-03-09 09:56:02 -0500

    Create reusable footer

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,9 @@
+var footer = '\
+<footer class="footer fixed-bottom mt-auto py-3 bg-light">\
+    <div class="container text-center">\
+      <span class="text-muted">Find on <a href="https://github.com/nastyhaze/gimbo-slice" target="_blank">GitHub</a></span><br>\
+      <span class="text-muted">Created by nastyhaze and MikeMcKew</span>\
+    </div>\
+  </footer>;'
+
+  document.write(footer);
