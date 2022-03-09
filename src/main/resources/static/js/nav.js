79d288214c8eb137cc916cfbbeb74dadcb4be084
Mike <m.p.mckew@gmail.com>
2022-03-09 09:34:18 -0500

    Add navbar injection functionality

---
+++ src/main/resources/static/js/nav.js
@@ -0,0 +1,35 @@
+var nav = '\
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">\
+<div class="container-fluid">\
+  <!-- <a class="navbar-brand" href="#">\
+      <img src="img/gimbo-placeholder.png" alt="gimbo-slice logo">\
+  </a> -->\
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+    <span class="navbar-toggler-icon"></span>\
+  </button>\
+  <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">\
+\
+    <!-- Nav items -->\
+    <ul class="navbar-nav mx-auto mb-2 mb-md-0">\
+      <li class="nav-item">\
+        <a class="nav-link active" aria-current="page" href="#">Home</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link active" href="#">Commands\
+        </a>\
+      </li>\
+      <li class="nav-item">\
+          <a class="nav-link active" href="a">About</a>\
+      </li>\
+    </ul>\
+\
+    <!-- Search bar -->\
+    <!-- <form class="d-flex">\
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">\
+      <button class="btn btn-outline-success" type="submit">Search</button>\
+    </form> -->\
+  </div>\
+</div>\
+</nav>';
+
+document.write(nav);
