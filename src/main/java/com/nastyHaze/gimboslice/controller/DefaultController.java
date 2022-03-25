a8554a9a568ff5d0ef87b8bc6e3e93632ca5afb9
danla <dan.lawrence.walker@gmail.com>
2022-03-25 12:12:38 -0700

    Jank junk from Dan

--- src/main/java/com/nastyHaze/gimboslice/controller/DefaultController.java
+++ src/main/java/com/nastyHaze/gimboslice/controller/DefaultController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/")
@@ -15,14 +18,58 @@ public class DefaultController {
     @Autowired
     private CommandRepository commandRepository;
 
-    /*@RequestMapping(value = "/index", method = RequestMethod.GET)
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "index";
-    }*/
+    }
 
-    @RequestMapping(value = "/commands", method = RequestMethod.GET)
+    // TEST SAMPLE CODE
+    public class Command {
+        int id;
+        String shortcut;
+        String response;
+
+        Command(int id, String shortcut, String response) {
+            this.id = id;
+            this.shortcut = shortcut;
+            this.response = response;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getShortcut() {
+            return shortcut;
+        }
+
+        public void setShortcut(String shortcut) {
+            this.shortcut = shortcut;
+        }
+
+        public String getResponse() {
+            return response;
+        }
+
+        public void setResponse(String response) {
+            this.response = response;
+        }
+    }
+
+    @RequestMapping(value = "commands", method = RequestMethod.GET)
     public String commandListing(Model model) {
-        model.addAttribute("commands", commandRepository.findAll());
+
+        List<Command> commands = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            Command temp = new Command(i, "shortcut: " + i + "\n", "response: " + i + "\n");
+            commands.add(temp);
+        }
+
+        model.addAttribute("commands", commands);
         return "commands";
     }
 }
