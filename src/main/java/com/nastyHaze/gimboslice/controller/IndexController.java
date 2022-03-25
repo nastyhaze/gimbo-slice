ef7e600cd0bac87a7b120437812d0fa9345bf9b1
Mike <m.p.mckew@gmail.com>
2022-03-25 14:24:51 -0400

    Move to controller folder

--- src/main/java/com/nastyHaze/gimboslice/controller/IndexController.java
+++ src/main/java/com/nastyHaze/gimboslice/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.nastyHaze.gimboslice;
+package com.nastyHaze.gimboslice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
