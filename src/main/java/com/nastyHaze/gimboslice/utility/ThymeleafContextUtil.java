e02521813423c4076d745222721dcb8ec4b140d5
danla <dan.lawrence.walker@gmail.com>
2022-04-13 10:07:42 -0700

    Initial commit - handle Thymeleaf Context provisioning

---
+++ src/main/java/com/nastyHaze/gimboslice/utility/ThymeleafContextUtil.java
@@ -0,0 +1,26 @@
+package com.nastyHaze.gimboslice.utility;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
+
+@Component
+public class ThymeleafContextUtil {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public Context getContext(Map<String, Object> valueMap) {
+        Context context = new Context();
+        context.setVariables(valueMap);
+        return context;
+    }
+
+    public String getParsedTemplate(String template, Context context) {
+        return templateEngine.process(template, context);
+    }
+}
