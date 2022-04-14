e5f07301d400abde3ac7bb28933d1786fd9de888
danla <dan.lawrence.walker@gmail.com>
2022-04-14 09:05:49 -0700

    JDOC & TODO

--- src/main/java/com/nastyHaze/gimboslice/controller/CommandListingController.java
+++ src/main/java/com/nastyHaze/gimboslice/controller/CommandListingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+/**
+ *  Controller class for Command Listing (retrieval) operations. Returns UI-friendly objects, rather than entities.
+ */
 @Controller
 @RequestMapping("/commands")
 public class CommandListingController {
@@ -16,9 +19,14 @@ public class CommandListingController {
     private CommandListingService commandListingService;
 
 
+    /**
+     * Returns all Commands from the DB.
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "/listing", method = RequestMethod.GET)
     public String commandsPage(Model model) {
-        model.addAttribute("commands", commandListingService.retrieveAllCommands());
+        model.addAttribute("commands", commandListingService.retrieveAllCommands()); // TODO: should only return active commands
 
         return "commands";
     }
