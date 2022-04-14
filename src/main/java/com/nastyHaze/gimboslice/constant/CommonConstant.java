01bc36b282897c035aeea024ba3dcb21c92fc9e7
danla <dan.lawrence.walker@gmail.com>
2022-04-14 10:19:11 -0700

    Emoji testing

--- src/main/java/com/nastyHaze/gimboslice/constant/CommonConstant.java
+++ src/main/java/com/nastyHaze/gimboslice/constant/CommonConstant.java
@@ -4,7 +4,7 @@ public class CommonConstant {
 
     public static final String INVALID_COMMAND_ERROR_MESSAGE = "Whoops! Invalid command, ***I D I O T***.";
 
-    public static final String INVALID_ARGUMENTS_ERROR_MESSAGE = "Issue with command arguments. Try checking your arguments ***N E R D***. \n:weary: \\:disaster: \\:lookofdisdain:";
+    public static final String INVALID_ARGUMENTS_ERROR_MESSAGE = "Issue with command arguments. Try checking your arguments ***N E R D***. \n:weary: <:disaster:642270718213488642> <:lookofdisdain:720104937224273982>";
 
     public static final String INFO_REQUEST = "info";
 }
