9056368b21fbce5a0188cc5c46c44f56d5bdac90
danla <dan.lawrence.walker@gmail.com>
2022-04-14 10:36:31 -0700

    JDOC

--- src/main/java/com/nastyHaze/gimboslice/entity/model/CommandDTO.java
+++ src/main/java/com/nastyHaze/gimboslice/entity/model/CommandDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ *  UI-Friendly version of Command Entity.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
