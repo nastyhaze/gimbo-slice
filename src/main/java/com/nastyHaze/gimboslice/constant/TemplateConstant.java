7a50988a093fdb8e0e93c6b313331a8b282f0d97
danla <dan.lawrence.walker@gmail.com>
2022-04-27 21:10:16 -0700

    removed unnecessary prefix from template paths

--- src/main/java/com/nastyHaze/gimboslice/constant/TemplateConstant.java
+++ src/main/java/com/nastyHaze/gimboslice/constant/TemplateConstant.java
@@ -2,17 +2,17 @@ package com.nastyHaze.gimboslice.constant;
 
 public class TemplateConstant {
 
-    public static final String INFO_TEMPLATE = "/commands/info.txt";
+    public static final String INFO_TEMPLATE = "commands/info.txt";
 
-    public static final String COMMANDS_TEMPLATE = "/commands/commands.txt";
+    public static final String COMMANDS_TEMPLATE = "commands/commands.txt";
 
-    public static final String GROUP_GOAL_TEMPLATE = "/commands/groupGoal.txt";
+    public static final String GROUP_GOAL_TEMPLATE = "commands/groupGoal.txt";
 
-    public static final String CLUELESS_TEMPLATE = "/commands/clueless.txt";
+    public static final String CLUELESS_TEMPLATE = "commands/clueless.txt";
 
-    public static final String CHAMBERS_REQ_TEMPLATE = "/commands/coxReq.txt";
+    public static final String CHAMBERS_REQ_TEMPLATE = "commands/coxReq.txt";
 
-    public static final String SLAYER_TASK_TEMPLATE = "/commands/slayerTask.txt";
+    public static final String SLAYER_TASK_TEMPLATE = "commands/slayerTask.txt";
 
-    public static final String BARROWS_TEMPLATE = "/commands/barrows.txt";
+    public static final String BARROWS_TEMPLATE = "commands/barrows.txt";
 }
